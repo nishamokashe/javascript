d72cd4b6db068e51da52536a4d20db6ade7c727f
nishamokashe <nishamokase18@gmail.com>
2023-05-08 21:55:41 +0530

    typeOf.js
    typeOf.js

---
+++ 03_typeOf.js
@@ -0,0 +1,36 @@
+console.log("Before Initialization");
+var bankName = "SBI Bank";
+console.log("Bank Name:", bankName);
+console.log('Data Type :',typeof bankName);
+console.log("After Initialization");
+var bankName = "SBI Bank";
+console.log("Bank Name:", bankName); 
+var typeName = typeof bankName;
+console.log("Data Type:", typeName); 
+console.log("----------------------------");
+var marks = "90%";
+console.log("Marks:", marks); 
+var typemarks = typeof marks;
+console.log("Data Type:", typemarks); 
+console.log("-----------------------------");
+console.log("Before Initialization");
+var isWorking;
+console.log("value:",typeof isWorking);
+console.log("After Initialization");
+var isWorking = true;
+console.log("value:",isWorking );
+console.log('Type of isWorking :',typeof isWorking);
+console.log("--------------------------------");
+var totalCount = "Hundread and Seven";
+console.log("value:", totalCount);
+console.log('Data Type :',typeof totalCount);
+holder = "107"; //variable update
+console.log("value",holder);
+console.log('Data Type :',typeof holder);
+
+
+
+
+
+
+
