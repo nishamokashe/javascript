e2309f1f6b69fc48c1c2a2a35a9df022d75967ab
nishamokashe <nishamokase18@gmail.com>
2023-05-08 21:51:50 +0530

    hello.js
    hello.js My First program in javaScript

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Nisha Mokashe..");
+
+var firstName = "Nisha";
+console.log(firstName);
+
+var age; //variable Declaration
+age = "22"; //variable Initilization
+console.log(age);
+
+var holder; //variable Declaration
+console.log(holder); //undefined
+holder = "My Spec"; //variable Initilization
+console.log(holder);
+
+holder = "Sachin Spec"; //variable update
+console.log(holder);
