16d8e009dcaa098d91c61a29341501e610410aac
nishamokashe <nishamokase18@gmail.com>
2023-05-08 21:54:37 +0530

    variableBasicAssignment.js
    variableBasicAssignment.js

---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,30 @@
+//variable Basic Assignment
+var rollNo = 102;
+console.log("Roll NO.:", rollNo);
+var typeRollNo = typeof rollNo;
+console.log("type of variable-->",typeRollNo);
+console.log("----------------------------------------");
+var studentName = "Swara";
+console.log("Name :", studentName); 
+var typeName = typeof studentName;
+console.log("type of variable-->", typeName); 
+console.log("----------------------------------------");
+var isBachelor = false; 
+console.log("Bachelor",isBachelor);
+var typeBachelor = typeof isBachelor;
+console.log("type :",typeBachelor);
+console.log("----------------------------------------");
+console.log("");
+
+
+console.log("Difference Between null and undefined:" );
+console.log("null:-->");
+var marks = null;
+console.log("Value of variable --> Marks:",marks);
+console.log('Type of variable Marks is:',typeof marks);
+console.log("----------------------------------------");
+ var address;
+ console.log("type of variable:",typeof address);
+ console.log('Type of variable Marks is:',typeof address);
+
+
