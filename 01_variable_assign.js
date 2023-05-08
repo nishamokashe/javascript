8496e123d05456c2524d0ef23441a9f19e08154c
nishamokashe <nishamokase18@gmail.com>
2023-05-08 21:53:55 +0530

    variable_assignment.js
    variable_assignment.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,14 @@
+var myFullName; //variable Declaration
+myFullName = "Nisha Mokashe"; //variable Declaration
+console.log(myFullName);
+
+holder = "Nisha Sudhakar Mokashe"; //variable update
+console.log(holder);
+
+var myLove; //variable Declaration
+myLove = "I love only JS"; //variable Declaration
+console.log(myLove);
+
+var listed;
+listed = "Nisha  Mokashe 22  At.Post Dabha  444701  Female  No";
+console.log(listed);
