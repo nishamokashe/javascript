30d5f679e0b08f52853dcb895609814d9c163e04
nishamokashe <nishamokase18@gmail.com>
2023-05-08 21:52:56 +0530

    primitiveTypes.js
    primitiveTypes.js

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,44 @@
+// Primitive Type
+// 1. Number
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+console.log(age, weight, height, speckNumber);
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+console.log(typeof speckNumber);
+
+
+console.log("Age:",age);
+console.log("Weight:",weight);
+console.log("Height:",height);
+console.log("SpeckNumber:",speckNumber);
+
+// 2. Boolean --> true or false
+var isMarried = true;
+var knowAngular = true; 
+var isSoftwareEngineer = true;
+var isBachelor = false; 
+console.log(isBachelor);
+
+var typeOfMarried = typeof isMarried;
+console.log(typeOfMarried);
+
+//3.String----> Sequence of character endclosed in '' or ""
+var firstName = "Bill";
+var position = 'Microsoft Co-founder';
+var typeOfName = typeof firstName;
+console.log("type of variable--> firstName", typeOfName);
+console.log("Type of variable --> position:",typeof position);
+
+//2. Special data type 
+//1.Undefined
+var address;
+console.log("type of variable address is",typeof address);
+
+//2.Null 
+var pin = null;
+console.log("Value of variable --> pin:",pin);
+console.log('Type of variable pin is:',typeof pin);
