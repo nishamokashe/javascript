5013361604ec74bce05d692cfe2af2ef2fd74bb3
nishamokashe <nishamokase18@gmail.com>
2023-05-15 09:06:39 +0530

    04_String.js
    04_StringBasicAndHandsOnAndTemplet.js

---
+++ 04_stringBasicAssignment.js
@@ -0,0 +1,11 @@
+function stringBasic(){
+    console.log("My dream company name is -->Infosys");
+}stringBasic();
+
+hobby1="reading";
+hobby2="dancing";
+hobby3="painting";
+console.log("my hobbies are:","1",hobby1 , "2",hobby2 ,  "3",hobby3);
+
+var result =hobby1.length+hobby1.length+hobby1.length;
+console.log("sum:",result);
---
+++ 04_stringHandsOn.js
@@ -0,0 +1,44 @@
+function stringHandson(){
+    string="    Hey you are doing good, keep it up   ";
+    console.log(string);}
+    stringHandson();
+    
+     
+    var string = "   Hey you are doing good, keep it up   ";
+    var stringLength = string.length;
+    console.log("Calculate len of string is: ", stringLength);
+    
+    var trimmedstring = string.trim();
+    var lengthAfterTrim = trimmedstring.length;
+    console.log("remove leading and trailing extra spaces: ",trimmedstring.length );
+    console.log(" total spaces : ", stringLength-lengthAfterTrim);
+    
+    var charIndex = string.charAt(2);
+    console.log("Char at starting index: ",charIndex);  
+    
+    var charAtLastIndex = string.charAt(string.length-4);
+    console.log("Char at last index: ",charAtLastIndex);
+    
+    
+    var resultSplit = string.split(" ");
+    console.log(resultSplit);
+    console.log("Total number of words: ",resultSplit.length);
+    
+    var indexOfGood = string.indexOf("good")
+    console.log("Index of good is: ", indexOfGood);
+    
+    var sliceResult =string.slice(22);
+    console.log("Slice Result is: ", sliceResult);
+    
+    var subStringResult = string.substring(22);
+    console.log("Substring result is: ", subStringResult);
+    
+    var result = string.startsWith("Hey");
+    console.log(` Is ${string} starts with character E: ${result}`);
+    
+    var result =string.endsWith("E");
+    console.log(` Is ${string} starts with character E: ${result}`);
+    
+    
+    var stringLength = string.length;
+    console.log("Calculate len of string is: ", stringLength);
