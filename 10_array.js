cf71bcfbfda421c011764b0fe879a8a0a37f784b
nishamokashe <nishamokase18@gmail.com>
2023-05-26 08:46:33 +0530

    10array.js
    10array.js

--- 10_array.js
+++ 10_array.js
@@ -133,3 +133,5 @@ console.log(`------ Resize an array -------`);
 var arrayNames = ["Anil", "Siya", "Ram" ,"Sunil", "Jenny"];
 arrayNames.length = 3;
 console.log(arrayNames);
+
+console.table(arrayNames);
